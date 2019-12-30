2f86d42b3a9952059053a11532f8512316bfae84
Phillip Schanely <pschanely+gitcmt@gmail.com>
2019-12-29 23:49:23 -0500

    watch command simplification

--- crosshair/main.py
+++ crosshair/main.py
@@ -26,21 +26,22 @@ from crosshair.util import debug, extract_module_from_file, set_debug, Crosshair
 
 
 def command_line_parser() -> argparse.ArgumentParser:
+    common = argparse.ArgumentParser(add_help=False)
+    common.add_argument('--verbose', '-v', action='store_true')
+    common.add_argument('--per_path_timeout', type=float)
+    common.add_argument('--per_condition_timeout', type=float)
     parser = argparse.ArgumentParser(description='CrossHair Analysis Tool')
-    parser.add_argument('--verbose', '-v', action='store_true')
-    parser.add_argument('--per_path_timeout', type=float)
-    parser.add_argument('--per_condition_timeout', type=float)
     subparsers = parser.add_subparsers(help='sub-command help', dest='action')
     check_parser = subparsers.add_parser(
-        'check', help='Analyze one or more files')
+        'check', help='Analyze one or more files', parents=[common])
     check_parser.add_argument('files', metavar='F', type=str, nargs='+',
                               help='files or fully qualified modules, classes, or functions')
     watch_parser = subparsers.add_parser(
-        'watch', help='Continuously watch and analyze files')
+        'watch', help='Continuously watch and analyze files', parents=[common])
     watch_parser.add_argument('files', metavar='F', type=str, nargs='+',
                               help='files or directories to analyze')
     showresults_parser = subparsers.add_parser(
-        'showresults', help='Display results from a currently running `watch` command')
+        'showresults', help='Display results from a currently running `watch` command', parents=[common])
     showresults_parser.add_argument('files', metavar='F', type=str, nargs='+',
                                     help='files or directories to analyze')
     return parser
@@ -82,7 +83,7 @@ class WatchedMember:
         return False
 
 
-WorkItemInput = Tuple[WatchedMember,
+WorkItemInput = Tuple[str, # (filename)
                       AnalysisOptions, float]  # (float is a deadline)
 WorkItemOutput = Tuple[WatchedMember, Counter[str], List[AnalysisMessage]]
 
@@ -96,23 +97,25 @@ def pool_worker_main(item: WorkItemInput, output: multiprocessing.queues.Queue) 
         if hasattr(os, 'nice'): # analysis should run at a low priority
             os.nice(10)
         set_debug(False)
-        member, options, deadline = item
+        filename, options, deadline = item
         stats: Counter[str] = Counter()
         options.stats = stats
+        _, module_name = extract_module_from_file(filename)
         try:
-            fn = member.get_member()
+            module = load_by_qualname(module_name)
         except NotFound:
             return
         except ErrorDuringImport as e:
             orig, frame = e.args
-            message = AnalysisMessage(MessageType.IMPORT_ERR, str(e), frame.filename, frame.lineno, 0, '')
-            output.put((member, stats, [message]))
+            message = AnalysisMessage(MessageType.IMPORT_ERR, str(orig), frame.filename, frame.lineno, 0, '')
+            output.put((filename, stats, [message]))
+            debug(f'Not analyzing "{filename}" because import failed: {e}')
             return
-        messages = analyze_any(fn, options)
-        output.put((member, stats, messages))
+        messages = analyze_any(module, options)
+        output.put((filename, stats, messages))
     except BaseException as e:
         raise CrosshairInternal(
-            'Worker failed while analyzing ' + member.qual_name) from e
+            'Worker failed while analyzing ' + filename) from e
 
 
 class Pool:
@@ -209,7 +212,6 @@ def walk_paths(paths: Iterable[str]) -> Iterable[str]:
 class Watcher:
     _paths: Set[str]
     _pool: Pool
-    _members: Dict[str, WatchedMember]
     _modtimes: Dict[str, float]
     _options: AnalysisOptions
     _next_file_check: float = 0.0
@@ -219,7 +221,6 @@ class Watcher:
         self._paths = set(files)
         self._state_updater = state_updater
         self._pool = self.startpool()
-        self._members = {}
         self._modtimes = {}
         self._options = options
         _ = list(walk_paths(self._paths)) # just to force an exit if we can't find a path
@@ -228,25 +229,17 @@ class Watcher:
         return Pool(multiprocessing.cpu_count() - 1)
 
     def run_iteration(self,
-                      all_members: List[WatchedMember],
-                      max_analyze_count=sys.maxsize,
                       max_condition_timeout=0.5) -> Iterator[
                           Tuple[Counter[str], List[AnalysisMessage]]]:
-        members = heapq.nlargest(
-            max_analyze_count, all_members, key=lambda m: m.last_modified)
-        debug(f'starting pass on {len(members)}/{len(all_members)} members, '
+        debug(f'starting pass '
               f'with a condition timeout of {max_condition_timeout}')
-
-        def timeout_for_position(pos: int) -> float:
-            return max(0.3, (1.0 - pos / max_analyze_count) * max_condition_timeout)
+        debug('Files:', self._modtimes.keys())
         pool = self._pool
-        for (index, member) in enumerate(members):
-            condition_timeout = timeout_for_position(index)
-            # TODO: wrong timeout for multiple conditions (or worse, classes)
-            worker_timeout = max(10.0, condition_timeout * 20.0)
+        for filename in self._modtimes.keys():
+            worker_timeout = max(10.0, max_condition_timeout * 20.0)
             options = dataclasses.replace(
-                self._options, per_condition_timeout=condition_timeout)
-            pool.submit((member, options, time.time() + worker_timeout))
+                self._options, per_condition_timeout=max_condition_timeout)
+            pool.submit((filename, options, time.time() + worker_timeout))
 
         pool.garden_workers()
         while pool.is_working():
@@ -256,19 +249,18 @@ class Watcher:
                 yield (counters, messages)
                 if pool.has_result():
                     continue
-            messages = self.check_changed()
-            if messages:
-                yield (Counter(), messages)
-            if self._change_flag:
+            change_detected = self.check_changed()
+            if change_detected:
+                self._change_flag = True
                 debug('Aborting iteration on change detection')
                 pool.terminate()
                 self._pool = self.startpool()
                 return
             pool.garden_workers()
         debug('Worker pool tasks complete')
-        yield (Counter(), self.check_changed())
+        yield (Counter(), [])
 
-    def run_watch_loop(self, starting_messages: Iterable[AnalysisMessage] = ()) -> NoReturn:
+    def run_watch_loop(self) -> NoReturn:
         restart = True
         stats: Counter[str] = Counter()
         active_messages: Dict[Tuple[str, int], AnalysisMessage]
@@ -276,29 +268,24 @@ class Watcher:
             if restart:
                 clear_screen()
                 clear_line('-')
-                line = f'  Analyzing {len(self._members)} classes/functions.          \r'
+                line = f'  Analyzing {len(self._modtimes)} files.          \r'
                 sys.stdout.write(color(line, AnsiColor.OKBLUE))
-                max_analyze_count = 5
                 max_condition_timeout = 0.5
                 restart = False
                 stats = Counter()
                 active_messages = {}
             else:
                 time.sleep(0.5)
-                max_analyze_count *= 2
                 max_condition_timeout *= 2
-            analyzed_members = list(self._members.values())
-            messages_merged(active_messages, starting_messages)
-            for curstats, messages in self.run_iteration(
-                    analyzed_members, max_analyze_count, max_condition_timeout):
+            for curstats, messages in self.run_iteration(max_condition_timeout):
                 debug('stats', curstats, messages)
                 stats.update(curstats)
                 if messages_merged(active_messages, messages):
                     self._state_updater.update(json.dumps({
                         'version': 1,
                         'time': time.time(),
-                        'members': [m.__dict__ for m in analyzed_members],
                         'messages': [m.toJSON() for m in active_messages.values()]}))
+                    linecache.checkcache()
                     clear_screen()
                     for message in active_messages.values():
                         lines = long_describe_message(message)
@@ -307,17 +294,17 @@ class Watcher:
                         clear_line('-')
                         print(lines, end='')
                     clear_line('-')
-                line = f'  Analyzed {stats["num_paths"]} paths in {len(self._members)} classes/functions.          \r'
+                line = f'  Analyzed {stats["num_paths"]} paths in {len(self._modtimes)} files.          \r'
                 sys.stdout.write(color(line, AnsiColor.OKBLUE))
             if self._change_flag:
                 self._change_flag = False
                 restart = True
-                line = f'  Restarting analysis over {len(self._members)} classes/functions.          \r'
+                line = f'  Restarting analysis over {len(self._modtimes)} files.          \r'
                 sys.stdout.write(color(line, AnsiColor.OKBLUE))
 
-    def check_changed(self) -> List[AnalysisMessage]:
+    def check_changed(self) -> bool:
         if time.time() < self._next_file_check:
-            return []
+            return False
         modtimes = self._modtimes
         changed = False
         for curfile in walk_paths(self._paths):
@@ -331,60 +318,8 @@ class Watcher:
                 modtimes[curfile] = cur_mtime
         self._next_file_check = time.time() + 1.0
         if not changed:
-            return []
-        # something changed, do a full member search
-        found_members = []
-        found_messages = []
-        for curfile in walk_paths(self._paths):
-            members, messages = self.check_file_changed(curfile)
-            found_members.extend(members)
-            found_messages.extend(messages)
-        my_members = self._members
-        # handle new/changed members
-        for wm in found_members:
-            if wm.qual_name in my_members:
-                my_members[wm.qual_name].consider_new(wm)
-            else:
-                my_members[wm.qual_name] = wm
-        # prune missing members:
-        found_member_names = set(m.qual_name for m in found_members)
-        for k in list(my_members.keys()):
-            if k not in found_member_names:
-                del my_members[k]
-        self._change_flag = True
-        return messages
-
-    def check_file_changed(self, curfile: str) -> Tuple[
-            List[WatchedMember], List[AnalysisMessage]]:
-        members = []
-        path_to_root, module_name = extract_module_from_file(curfile)
-        debug(f'Attempting to import {curfile} as module "{module_name}"')
-        if path_to_root not in sys.path:
-            sys.path.append(path_to_root)
-        try:
-            module = importlib.import_module(module_name)
-            importlib.reload(module)
-        except Exception:
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            assert exc_traceback is not None
-            lineno = exception_line_in_file(
-                traceback.extract_tb(exc_traceback), curfile)
-            if lineno is None:
-                lineno = 1
-            debug(f'Unable to load module "{module_name}" '
-                  f'in {curfile}: {exc_type}: {exc_value}')
-            return ([], [AnalysisMessage(MessageType.IMPORT_ERR,
-                                         str(exc_value), curfile, lineno, 0, '')])
-
-        for (name, member) in analyzable_members(module):
-            qualname = module.__name__ + '.' + member.__name__
-            try:
-                src = inspect.getsource(member)
-            except OSError:
-                debug(f'Could not find member "{member}"')
-                continue
-            members.append(WatchedMember(qualname, src))
-        return (members, [])
+            return False
+        return True
 
 
 def clear_screen():
@@ -431,8 +366,8 @@ def watch(args: argparse.Namespace, options: AnalysisOptions) -> int:
     try:
         with StateUpdater() as state_updater:
             watcher = Watcher(options, args.files, state_updater)
-            messages = watcher.check_changed()
-            watcher.run_watch_loop(messages)
+            watcher.check_changed()
+            watcher.run_watch_loop()
     except KeyboardInterrupt:
         watcher._pool.terminate()
         print()
@@ -446,6 +381,8 @@ def format_src_context(filename: str, lineno: int) -> str:
     output = [f'{filename}:{lineno}:\n']
     for curline in line_numbers:
         text = linecache.getline(filename, curline)
+        if text == '': # (actual empty lines have a newline)
+            continue
         output.append('>' + color(text, AnsiColor.WARNING)
                       if lineno == curline else '|' + text)
     return ''.join(output)
