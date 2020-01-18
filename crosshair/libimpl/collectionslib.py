7a4e4606fdbbb6c34825ae79e5c25af4ca5ff528
Phillip Schanely <pschanely+gitcmt@gmail.com>
2020-01-18 00:05:51 -0800

    Add skeleton for deque implementation

--- crosshair/libimpl/collectionslib.py
+++ crosshair/libimpl/collectionslib.py
@@ -3,24 +3,40 @@ from typing import *
 
 from crosshair import register_type
 
+T = TypeVar('T')
+class ListBasedDeque:
+    def __init__(self, contents: List[T], maxlen: Optional[int]=None):
+        self._contents = contents
+        self._maxlen = maxlen
+    def _has_room(self) -> bool:
+        maxlen = self._maxlen
+        return maxlen is None or len(self._contents) < maxlen
+    def append_left(self, item: T):
+        if self._has_room():
+            self._contents = [item] + self._contents
+    # TODO: implement remainder of deque ...
+
 
 def make_registrations():
-    # NOTE: could be symbolic (but note the default_factory is changable/stateful):
+    # NOTE: defaultdict could be symbolic (but note the default_factory is changable/stateful):
     register_type(collections.defaultdict, lambda p, kt=Any, vt=Any: collections.defaultdict(p(Callable[[], vt]), p(Dict[kt, vt]))) # type: ignore
     register_type(collections.ChainMap, lambda p, kt=Any, vt=Any: collections.ChainMap(*p(Tuple[Dict[kt, vt], ...]))) # type: ignore
     register_type(collections.abc.Mapping, lambda p, t=Any: p(Dict[t]))  # type: ignore
     register_type(collections.abc.MutableMapping, lambda p, t=Any: p(Dict[t]))  # type: ignore
     register_type(collections.OrderedDict, lambda p, kt=Any, vt=Any: collections.OrderedDict(p(Dict[kt, vt]))) # type: ignore
     register_type(collections.Counter, lambda p, t=Any: collections.Counter(p(Dict[t, int]))) # type: ignore
-    # MappingView: (as instantiated origin)
+    # TODO: MappingView is missing
     register_type(collections.abc.ItemsView, lambda p, kt=Any, vt=Any: p(Set[Tuple[kt, vt]]))  # type: ignore
     register_type(collections.abc.KeysView, lambda p, t=Any: p(Set[t]))  # type: ignore
     register_type(collections.abc.ValuesView, lambda p, t=Any: p(Set[t]))  # type: ignore
 
     register_type(collections.abc.Container, lambda p, t=Any: p(Tuple[t, ...]))
     register_type(collections.abc.Collection, lambda p, t=Any: p(Tuple[t, ...]))
-    # TODO: a custom impl in simplestructs.py
+
     register_type(collections.deque, lambda p, t=Any: collections.deque(p(Tuple[t, ...])))
+    # TODO: When ready, use custom deque class instead:
+    # register_type(collections.deque, lambda p, t=Any: ListBasedDeque(p(Tuple[t, ...])))
+
     register_type(collections.abc.Iterable, lambda p, t=Any: p(Tuple[t, ...]))
     register_type(collections.abc.Iterator, lambda p, t=Any: iter(p(Iterable[t])))  # type: ignore
 
---
+++ crosshair/libimpl/collectionslib_test.py
@@ -0,0 +1,42 @@
+import sys
+import unittest
+from typing import *
+
+from crosshair.core_and_libs import *
+from crosshair.test_util import check_ok
+from crosshair.test_util import check_exec_err
+from crosshair.test_util import check_post_err
+from crosshair.test_util import check_fail
+from crosshair.test_util import check_unknown
+from crosshair.test_util import check_messages
+from crosshair.util import set_debug
+
+class CollectionsLibTests(unittest.TestCase):
+    # Remove these TODO prefixes to enable the tests:
+    def TODO_test_deque_len_ok(self) -> None:
+        def f(l: Deque[int]) -> Deque[int]:
+            '''
+            post: len(_) == len(__old__.l) + 1
+            '''
+            l.append(42)
+            return l
+        # Right now, this returns unknown rather than ok, because creation of
+        # the deque forces the list length to become concrete.
+        self.assertEqual(*check_ok(f))
+
+    def TODO_test_deque_len_fail(self) -> None:
+        def f(l: Deque[int]) -> Deque[int]:
+            '''
+            pre: len(l) > 0
+            post: len(l) != 222
+            '''
+            return l
+        # Right now, this returns unknown rather than fail, because creation of
+        # the deque forces the list length to become concrete.
+        self.assertEqual(*check_fail(f))
+
+if __name__ == '__main__':
+    if ('-v' in sys.argv) or ('--verbose' in sys.argv):
+        set_debug(True)
+    else:
+        unittest.main()
