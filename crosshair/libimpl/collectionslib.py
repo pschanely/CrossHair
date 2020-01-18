20a20bbdec7089a6ce13ad2a24aaa75066797394
Phillip Schanely <pschanely+gitcmt@gmail.com>
2020-01-18 00:07:53 -0800

    Use a list for the list-based deque

--- crosshair/libimpl/collectionslib.py
+++ crosshair/libimpl/collectionslib.py
@@ -35,7 +35,7 @@ def make_registrations():
 
     register_type(collections.deque, lambda p, t=Any: collections.deque(p(Tuple[t, ...])))
     # TODO: When ready, use custom deque class instead:
-    # register_type(collections.deque, lambda p, t=Any: ListBasedDeque(p(Tuple[t, ...])))
+    # register_type(collections.deque, lambda p, t=Any: ListBasedDeque(p(List[t, ...]))) # type: ignore
 
     register_type(collections.abc.Iterable, lambda p, t=Any: p(Tuple[t, ...]))
     register_type(collections.abc.Iterator, lambda p, t=Any: iter(p(Iterable[t])))  # type: ignore
