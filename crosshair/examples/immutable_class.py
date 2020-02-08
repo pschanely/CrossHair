9db92bb4a21605fb7ff46cf63e71626b4c4f4672
Phillip Schanely <pschanely@gmail.com>
2020-02-07 20:37:25 -0500

    Define __old__ in preconditions

--- crosshair/examples/immutable_class.py
+++ crosshair/examples/immutable_class.py
@@ -2,9 +2,6 @@ class Immutable:
     '''
     A mixin to enforce that instaces are immutable.
     inv: self.__dict__ == __old__.self.__dict__
-
-    WARNING: CrossHair implements __old__ with deep-copy-like logic.
-    So this mixin is only useful if your class implements __eq__().
     '''
 
 class Apples(Immutable):
@@ -14,7 +11,5 @@ class Apples(Immutable):
     count: int
     def buy_one_more(self) -> int:
         self.count += 1
-    def __eq__(self, other: object) -> bool:
-        return isinstance(other, Apples) and self.count == other.count
     def __repr__(self):
         return f'Apples({self.count!r})'
