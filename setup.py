e61f1adfb3c1bb21c4c932656e1d5ca2b3df0c93
Phillip Schanely <pschanely@gmail.com>
2020-03-01 12:11:03 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import unittest
 
 setup(
     name='crosshair-tool',
-    version='0.0.2',
+    version='0.0.3',
     author='Phillip Schanely',
     author_email='pschanely+vE7F@gmail.com',
     packages=find_packages(),
