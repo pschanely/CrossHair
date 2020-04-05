8e617ae1ffa6eb4f7e9c935cbf685d938a688ce0
oneEdoubleD <45830152+oneEdoubleD@users.noreply.github.com>
2020-04-05 18:17:08 +0100

    Implemented collections deque (#13)
    * Symbolic collections.deque class added. One issue with rotate and negative arguments left to fix.
    
    * Fixed the issues with rotate()

--- crosshair/libimpl/collectionslib.py
+++ crosshair/libimpl/collectionslib.py
@@ -8,14 +8,71 @@ class ListBasedDeque:
     def __init__(self, contents: List[T], maxlen: Optional[int]=None):
         self._contents = contents
         self._maxlen = maxlen
+    
+    def __len__(self) -> int:
+        return len(self._contents)
+
     def _has_room(self) -> bool:
         maxlen = self._maxlen
         return maxlen is None or len(self._contents) < maxlen
-    def append_left(self, item: T):
+
+    def appendleft(self, item: T) -> None:
         if self._has_room():
             self._contents = [item] + self._contents
-    # TODO: implement remainder of deque ...
+        else:
+            del self._contents[-1]
+            self._contents = [item] + self._contents
 
+    def append(self, item: T) -> None:
+        if self._has_room():
+            self._contents = self._contents + [item]
+        else:
+            del self._contents[0]
+            self._contents = self._contents + [item]
+
+    def clear(self) -> None:
+        self._contents = []
+
+    def count(self, item: T) -> int:
+        c = 0
+        for i in self._contents:
+            if i == item:
+                c += 1
+        return c
+
+    def index(self, item :T, start: Optional[int]=None, end: Optional[int]=None) -> int:
+        if start is not None and end is None:
+            return self._contents.index(item, start)
+        if start is not None and end is not None:
+            return self._contents.index(item, start, end)
+        return self._contents.index(item)
+
+    def insert(self, index: int, item: T) -> None:
+        self._contents.insert(index, item)
+    
+    def pop(self) -> T:
+        x = self._contents[-1]
+        del self._contents[-1]
+        return x
+
+    def popleft(self) -> T:
+        x = self._contents[0]
+        del self._contents[0]
+        return x
+    
+    def remove(self, item: T) -> None:
+        self._contents.remove(item)
+
+    def reverse(self) -> None:
+        self._contents.reverse()
+
+    def rotate(self, n: Optional[int]=1) -> None:
+            if not self._contents or n % len(self._contents) == 0:
+                pass
+            self._contents = self._contents[-n % len(self._contents):] + self._contents[:-n % len(self._contents)]
+
+    def maxlen(self) -> int:
+        return self._maxlen
 
 def make_registrations():
     # NOTE: defaultdict could be symbolic (but note the default_factory is changable/stateful):
@@ -33,9 +90,7 @@ def make_registrations():
     register_type(collections.abc.Container, lambda p, t=Any: p(Tuple[t, ...]))
     register_type(collections.abc.Collection, lambda p, t=Any: p(Tuple[t, ...]))
 
-    register_type(collections.deque, lambda p, t=Any: collections.deque(p(Tuple[t, ...])))
-    # TODO: When ready, use custom deque class instead:
-    # register_type(collections.deque, lambda p, t=Any: ListBasedDeque(p(List[t, ...]))) # type: ignore
+    register_type(collections.deque, lambda p, t=Any: ListBasedDeque(p(List[t, ...]))) # type: ignore
 
     register_type(collections.abc.Iterable, lambda p, t=Any: p(Tuple[t, ...]))
     register_type(collections.abc.Iterator, lambda p, t=Any: iter(p(Iterable[t])))  # type: ignore
--- crosshair/libimpl/collectionslib_test.py
+++ crosshair/libimpl/collectionslib_test.py
@@ -3,6 +3,7 @@ import unittest
 from typing import *
 
 from crosshair.core_and_libs import *
+from crosshair.libimpl.collectionslib import ListBasedDeque
 from crosshair.test_util import check_ok
 from crosshair.test_util import check_exec_err
 from crosshair.test_util import check_post_err
@@ -12,28 +13,102 @@ from crosshair.test_util import check_messages
 from crosshair.util import set_debug
 
 class CollectionsLibTests(unittest.TestCase):
-    # Remove these TODO prefixes to enable the tests:
-    def TODO_test_deque_len_ok(self) -> None:
-        def f(l: Deque[int]) -> Deque[int]:
-            '''
-            post: len(_) == len(__old__.l) + 1
-            '''
-            l.append(42)
-            return l
-        # Right now, this returns unknown rather than ok, because creation of
-        # the deque forces the list length to become concrete.
-        self.assertEqual(*check_ok(f))
 
-    def TODO_test_deque_len_fail(self) -> None:
-        def f(l: Deque[int]) -> Deque[int]:
-            '''
-            pre: len(l) > 0
-            post: len(l) != 222
-            '''
-            return l
-        # Right now, this returns unknown rather than fail, because creation of
-        # the deque forces the list length to become concrete.
-        self.assertEqual(*check_fail(f))
+    def setUp(self):
+        self.test_list = ListBasedDeque([1, 2, 3, 4, 5])
+
+    def test_deque_appendleft(self) -> None:
+        self.test_list.appendleft(0)
+        self.assertEqual(self.test_list.popleft(), 0)
+
+    def test_deque_appendleft_with_full_deque(self) -> None:
+        temp_list = ListBasedDeque([1, 2, 3, 4, 5], maxlen=5)
+        temp_list.appendleft(42)
+        self.assertTrue(temp_list.popleft() == 42 and temp_list.pop() == 4)
+    
+    def test_deque_appendleft_doesnt_increase_size_with_maxlen(self) -> None:
+        temp_list = ListBasedDeque([1, 2, 3, 4, 5], maxlen=5)
+        temp_list.appendleft(42)
+        self.assertTrue(len(temp_list) == temp_list.maxlen())
+
+    def test_deque_append(self) -> None:
+        self.test_list.append(0)
+        self.assertEqual(self.test_list.pop(), 0)
+
+    def test_deque_append_with_full_deque(self) -> None:
+        temp_list = ListBasedDeque([1, 2, 3, 4, 5], maxlen=5)
+        temp_list.append(42)
+        self.assertTrue(temp_list.pop() == 42 and temp_list.popleft() == 2)
+    
+    def test_deque_append_doesnt_increase_size_with_maxlen(self) -> None:
+        temp_list = ListBasedDeque([1, 2, 3, 4, 5], maxlen=5)
+        temp_list.append(42)
+        self.assertTrue(len(temp_list) == temp_list.maxlen())
+
+    def test_deque_clear(self) -> None:
+        self.test_list.clear()
+        self.assertEqual(len(self.test_list), 0)
+
+    def test_deque_count(self) -> None:
+        self.test_list.append(1)
+        count = self.test_list.count(1)
+        self.assertEqual(count, 2)
+
+    def test_deque_index(self) -> None:
+        i = self.test_list.index(5)
+        self.assertEqual(i, 4)
+
+    def test_deque_index_with_start_index(self) -> None:
+        i = self.test_list.index(5, start=1)
+        self.assertEqual(i, 4)
+
+    def test_deque_index_with_start_index_throws_correct_exception(self) -> None:
+        with self.assertRaises(ValueError) as context:
+            self.test_list.index(1, start=2)
+
+        self.assertTrue('1 is not in list' in str(context.exception))
+    
+    def test_deque_index_with_start_and_end_index(self) -> None:
+        i = self.test_list.index(2, start=0, end=3)
+        self.assertEqual(i, 1)
+    
+    def test_deque_index_with_start_and_end_index_throws_correct_exception(self) -> None:
+        with self.assertRaises(ValueError) as context:
+            self.test_list.index(6, start=0, end=1)
+
+        self.assertTrue('6 is not in list' in str(context.exception))
+
+    def test_deque_insert(self) -> None:
+        self.test_list.insert(index=1, item=42)
+        self.assertEqual(self.test_list.index(42), 1)
+
+    def test_deque_pop(self) -> None:
+        self.assertEqual(self.test_list.pop(), 5)
+
+    def test_deque_popleft(self) -> None:
+        self.assertEqual(self.test_list.popleft(), 1)
+
+    def test_deque_remove(self) -> None:
+        original_length = len(self.test_list)
+        self.test_list.remove(1)
+        self.assertTrue(len(self.test_list) == original_length - 1)
+
+    def test_deque_reverse(self) -> None:
+        self.test_list.reverse()
+        self.assertTrue(self.test_list.popleft() == 5)
+
+    def test_deque_rotate(self) -> None:
+        self.test_list.rotate(n=1)
+        self.assertTrue(self.test_list.popleft() == 5)
+
+    def test_deque_rotate_left(self) -> None:
+         self.test_list.rotate(n=-1)
+         self.assertTrue(self.test_list.popleft() == 2)
+
+    def test_deque_maxlen(self) -> None:
+        l = ListBasedDeque([1, 2, 3], 5)
+        self.assertTrue(l.maxlen() == 5)
+
 
 if __name__ == '__main__':
     if ('-v' in sys.argv) or ('--verbose' in sys.argv):
