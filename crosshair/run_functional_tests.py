5d1cfdcac5305ff59180b029dd3e7e096274a594
Marko Ristin <marko@ristin.ch>
2021-02-03 18:46:10 +0100

    Normalize paths in golden standard of functional tests (#64)
    * Normalize paths in golden standard of functional tests
    
    * Update crosshair/run_functional_tests.py

--- crosshair/run_functional_tests.py
+++ crosshair/run_functional_tests.py
@@ -3,6 +3,7 @@ import argparse
 import fnmatch
 import os
 import pathlib
+import re
 import shlex
 import subprocess
 import sys
@@ -187,8 +188,12 @@ def main() -> int:
                 # with a place holder to make these tests machine agnostic.
                 ##
 
-                stdout = stdout.replace(str(examples_dir), "<examples dir>")
-                stderr = stderr.replace(str(examples_dir), "<examples dir>")
+                path_re = re.compile(r'^.*[/\\]([_\w]+\.py):')
+
+                stdout = path_re.sub(
+                    r'<path prefix>/\1:', stdout)
+
+                stderr = path_re.sub(r'<path prefix>/\1:', stderr)
 
                 if overwrite:
                     expected_stdout_pth.write_text(stdout)
