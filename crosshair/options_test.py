6292b950bd8b910fde10fb61fe5c6384409426b0
Phillip Schanely <pschanely@gmail.com>
2021-02-11 11:10:18 -0500

    Start a test file for options.py

---
+++ crosshair/options_test.py
@@ -0,0 +1,9 @@
+from crosshair.options import AnalysisOptions
+
+def test_AnalysisOptions_split_limits() -> None:
+    options = AnalysisOptions(per_path_timeout=10.0, max_iterations=16)
+    part1, part2 = options.split_limits(0.1)
+    assert part1.per_path_timeout == 1.0
+    assert part2.per_path_timeout == 9.0
+    assert part1.max_iterations == 2
+    assert part2.max_iterations == 14
