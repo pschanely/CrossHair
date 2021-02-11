1b35359daedf062969fc708a01d6a85a223bc222
Phillip Schanely <pschanely@gmail.com>
2021-02-11 11:05:01 -0500

    Add typing annotations for sphinx's conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+from typing import Dict, List
 
 sys.path.insert(0, os.path.abspath("../.."))
 
@@ -78,7 +79,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
@@ -99,7 +100,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path: List[str] = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -120,7 +121,7 @@ htmlhelp_basename = "crosshairdoc"
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {
+latex_elements: Dict[str, str] = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
