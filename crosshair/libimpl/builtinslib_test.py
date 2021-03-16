1d1537071094c1de6d1df4882b0a59f8eaade657
Rik <rik.de.kort@hey.com>
2021-03-16 18:26:40 +0100

    Add tests for rfind and find

--- crosshair/libimpl/builtinslib_test.py
+++ crosshair/libimpl/builtinslib_test.py
@@ -434,6 +434,27 @@ class StringsTest(unittest.TestCase):
 
         self.assertEqual(*check_ok(f))
 
+    def test_find_with_negative_limits_ok(self) -> None:
+        def f(a: str) -> int:
+            """ post: _ == -1 """
+            return a.find("abc", -2, 3)
+
+        self.assertEqual(*check_ok(f))
+
+    def test_rfind_with_limits_ok(self) -> None:
+        def f(a: str) -> int:
+            """ post: _ == -1 """
+            return a.rfind("abc", 1, 3)
+
+        self.assertEqual(*check_ok(f))
+
+    def test_rfind_with_negative_limits_ok(self) -> None:
+        def f(a: str) -> int:
+            """ post: _ == -1 """
+            return a.rfind("abc", -2, 3)
+
+        self.assertEqual(*check_ok(f))
+
     def test_index_err(self) -> None:
         def f(s1: str, s2: str) -> int:
             """
