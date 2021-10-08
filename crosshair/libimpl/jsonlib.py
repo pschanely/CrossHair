6cc9418a018d7fb8a5e149fdc58adba1035e0189
Phillip Schanely <pschanely@gmail.com>
2021-10-08 12:47:42 -0400

    Use the pure-Python json encoders and decoders #30

---
+++ crosshair/libimpl/jsonlib.py
@@ -0,0 +1,16 @@
+import importlib
+import sys
+
+
+def make_registrations() -> None:
+    # CPython already ships with a pure-Python implementation of this module.
+    # We just need to do some hacks to prevent it from using the optimized C versions
+    # of some functions:
+    import json.encoder
+    import json.decoder
+    import json.scanner  # type: ignore
+
+    sys.modules["_json"] = None  # type: ignore
+    importlib.reload(json.encoder)
+    importlib.reload(json.decoder)
+    importlib.reload(json.scanner)
---
+++ crosshair/libimpl/jsonlib_ch_test.py
@@ -0,0 +1,35 @@
+import json
+from typing import *
+import sys
+
+import pytest  # type: ignore
+
+from crosshair.core_and_libs import analyze_function
+from crosshair.core_and_libs import run_checkables
+from crosshair.core_and_libs import MessageType
+from crosshair.options import AnalysisOptionSet
+from crosshair.test_util import compare_results
+
+
+def TODO_check_decode(s: str):  # TODO: doesn't work yet
+    """ post: _ """
+    return compare_results(json.loads, s)
+
+
+# TODO: Most types don't encode correctly yet
+def check_encode(obj: Union[str]):
+    """ post: _ """
+    return compare_results(json.dumps, obj)
+
+
+# This is the only real test definition.
+# It runs crosshair on each of the "check" functions defined above.
+@pytest.mark.parametrize("fn_name", [fn for fn in dir() if fn.startswith("check_")])
+def test_builtin(fn_name: str) -> None:
+    opts = AnalysisOptionSet(
+        max_iterations=7, per_condition_timeout=20, per_path_timeout=5
+    )
+    fn = getattr(sys.modules[__name__], fn_name)
+    messages = run_checkables(analyze_function(fn, opts))
+    errors = [m for m in messages if m.state > MessageType.PRE_UNSAT]
+    assert errors == []
