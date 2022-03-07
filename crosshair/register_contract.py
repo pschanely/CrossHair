0692f1ffbe53a1a852a349409784de335d9db152
Loïc Montandon <loic.montandon@bluewin.ch>
2022-03-07 07:43:49 +0100

    Check signature of registered contracts

--- crosshair/register_contract.py
+++ crosshair/register_contract.py
@@ -1,6 +1,11 @@
 """API for registering contracts for external libraries."""
 from dataclasses import dataclass
+from inspect import signature
 from typing import Callable, Dict, Optional
+
+
+class ContractRegistrationError(Exception):
+    pass
 
 
 @dataclass
@@ -12,11 +17,33 @@ class Contract:
 REGISTERED_CONTRACTS: Dict[str, Contract] = {}
 
 
+def _verify_signatures(
+    fn: Callable,
+    pre: Optional[Callable[..., bool]],
+    post: Optional[Callable[..., bool]],
+) -> None:
+    """Verify if pre- and post-condition signatures are valid."""
+    fn_params = set(signature(fn).parameters.keys())
+    if pre:
+        pre_params = set(signature(pre).parameters.keys())
+        if not pre_params <= fn_params:
+            raise ContractRegistrationError(
+                f"Malformated precondition for function {fn.__name__}. Unexpected arguments: {pre_params - fn_params}"
+            )
+    if post:
+        post_params = set(signature(post).parameters.keys())
+        fn_params.add("result")
+        if not post_params <= fn_params:
+            raise ContractRegistrationError(
+                f"Malformated postcondition for function {fn.__name__}. Unexpected parameters: {post_params - fn_params}"
+            )
+
+
 def register_contract(
     fn: Callable,
     *,
     pre: Optional[Callable[..., bool]] = None,
-    post: Optional[Callable[..., bool]] = None
+    post: Optional[Callable[..., bool]] = None,
 ) -> None:
     """
     Register a contract for the given function.
@@ -24,7 +51,9 @@ def register_contract(
     :param fn: The function to add a contract for.
     :param pre: The preconditon which should hold when entering the function.
     :param post: The postcondition which should hold when returning from the function.
+    :raise: `ContractRegistrationError` if the registered contract is malformed.
     """
+    _verify_signatures(fn, pre, post)
     REGISTERED_CONTRACTS[fn.__module__ + fn.__name__] = Contract(pre, post)
 
 
