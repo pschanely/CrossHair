a26cd15dca7e33625fb189a5d70f6bb5570301c0
Phillip Schanely <pschanely@gmail.com>
2022-10-03 16:20:47 -0400

    Convert datetimelib_test to pytest

--- crosshair/libimpl/datetimelib_test.py
+++ crosshair/libimpl/datetimelib_test.py
@@ -1,104 +1,107 @@
 import datetime
-import sys
-import unittest
+
+import pytest
 
 from crosshair.options import AnalysisOptionSet
-from crosshair.statespace import CANNOT_CONFIRM, EXEC_ERR, POST_FAIL, MessageType
+from crosshair.statespace import CANNOT_CONFIRM, EXEC_ERR, POST_FAIL
 from crosshair.test_util import check_states
-from crosshair.util import set_debug
 
 _SLOW_TEST = AnalysisOptionSet(per_condition_timeout=10, per_path_timeout=5)
 
 
-class DatetimeLibTests(unittest.TestCase):
-    def test_symbolic_months_fail(self) -> None:
-        def f(num_months: int) -> datetime.date:
-            """
-            pre: 0 <= num_months <= 100
-            post: _.year != 2003
-            """
-            dt = datetime.date(2000, 1, 1)
-            return dt + datetime.timedelta(days=30 * num_months)
+def test_timedelta_symbolic_months_fail() -> None:
+    def f(num_months: int) -> datetime.date:
+        """
+        pre: 0 <= num_months <= 100
+        post: _.year != 2003
+        """
+        dt = datetime.date(2000, 1, 1)
+        return dt + datetime.timedelta(days=30 * num_months)
 
-        check_states(f, POST_FAIL, _SLOW_TEST)
+    check_states(f, POST_FAIL, _SLOW_TEST)
 
-    def test_date_fail(self) -> None:
-        def f(dt: datetime.date) -> int:
-            """
-            post: _ != 2020
-            """
-            return dt.year
+@pytest.mark.demo
+def test_date___init___method() -> None:
+    def f(dt: datetime.date) -> datetime.date:
+        """
+        Is February 29th ever part of a valid date?
+        (it is on leap years)
+        
+        post: (dt.month, dt.day) != (2, 29)
+        """
+        return dt
 
-        check_states(f, POST_FAIL)
+    check_states(f, POST_FAIL)
 
-    def test_time_fail(self) -> None:
-        def f(dt: datetime.time) -> int:
-            """
-            post: _ != 14
-            """
-            return dt.hour
+def test_time_fail() -> None:
+    def f(dt: datetime.time) -> int:
+        """
+        post: _ != 14
+        """
+        return dt.hour
 
-        check_states(f, POST_FAIL)
+    check_states(f, POST_FAIL)
 
-    def test_datetime_fail(self) -> None:
-        def f(dtime: datetime.datetime) -> int:
-            """
-            post: _ != 22
-            """
-            return dtime.second
+def test_datetime_fail() -> None:
+    def f(dtime: datetime.datetime) -> int:
+        """
+        post: _ != 22
+        """
+        return dtime.second
 
-        check_states(f, POST_FAIL, AnalysisOptionSet(max_iterations=60))
+    check_states(f, POST_FAIL, AnalysisOptionSet(max_iterations=60))
 
-    def test_timedelta_fail(self) -> None:
-        def f(d: datetime.timedelta) -> int:
-            """
-            post: _ != 9
-            """
-            return d.seconds
+def test_timedelta_fail() -> None:
+    def f(d: datetime.timedelta) -> int:
+        """
+        post: _ != 9
+        """
+        return d.seconds
 
-        check_states(f, POST_FAIL)
+    check_states(f, POST_FAIL)
 
-    def test_date_plus_delta_unknown(self) -> None:
-        def f(delta: datetime.timedelta) -> datetime.date:
-            """
-            post: _.year != -9999
-            raises: OverflowError
-            """
-            return datetime.date(2000, 1, 1) + delta
+def test_date_plus_delta_unknown() -> None:
+    def f(delta: datetime.timedelta) -> datetime.date:
+        """
+        post: _.year != -9999
+        raises: OverflowError
+        """
+        return datetime.date(2000, 1, 1) + delta
 
-        check_states(f, CANNOT_CONFIRM)
+    check_states(f, CANNOT_CONFIRM)
 
-    def test_date_plus_delta_overflow_err(self) -> None:
-        def f(delta: datetime.timedelta) -> datetime.date:
-            """
-            post: _.year != -9999
-            """
-            return datetime.date(2000, 1, 1) + delta
+def test_date_plus_delta_overflow_err() -> None:
+    def f(delta: datetime.timedelta) -> datetime.date:
+        """
+        post: _.year != -9999
+        """
+        return datetime.date(2000, 1, 1) + delta
 
-        check_states(f, EXEC_ERR)
+    check_states(f, EXEC_ERR)
 
-    def test_date_plus_delta_fail(self) -> None:
-        def f(delta: datetime.timedelta) -> datetime.date:
-            """
-            post: _.year != 2001
-            raises: OverflowError
-            """
-            return datetime.date(2000, 1, 1) + delta
+@pytest.mark.demo("yellow")
+def test_timedelta___add___method() -> None:
+    def f(delta: datetime.timedelta) -> datetime.date:
+        """
+        Can we increment 2000-01-01 by some time delta to reach 2001?
 
-        check_states(f, POST_FAIL, _SLOW_TEST)
+        NOTE: Although this counterexample is found relatively quickly,
+        most date arithmetic solutions will require at least a minute
+        of processing time.
 
-    def TODO_test_leap_year(self) -> None:
-        # The solver returns unknown when adding a delta to a symbolic date. (nonlinear I think)
-        def f(start: datetime.date) -> datetime.date:
-            """
-            post: _.year == start.year + 1
-            """
-            return start + datetime.timedelta(days=365)
+        post: _.year != 2001
+        raises: OverflowError
+        """
+        return datetime.date(2000, 1, 1) + delta
 
-        check_states(f, POST_FAIL)
+    check_states(f, POST_FAIL)
 
+def TODO_test_leap_year() -> None:
+    # The solver returns unknown when adding a delta to a symbolic date. (nonlinear I think)
+    def f(start: datetime.date) -> datetime.date:
+        """
+        post: _.year == start.year + 1
+        """
+        return start + datetime.timedelta(days=365)
 
-if __name__ == "__main__":
-    if ("-v" in sys.argv) or ("--verbose" in sys.argv):
-        set_debug(True)
-    unittest.main()
+    check_states(f, POST_FAIL)
