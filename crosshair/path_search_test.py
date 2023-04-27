0db82a0b4c57639139b5a0a302637b5db5891aef
Phillip Schanely <pschanely@gmail.com>
2023-04-27 18:01:08 -0400

    Allow more freedom in simplified test result

--- crosshair/path_search_test.py
+++ crosshair/path_search_test.py
@@ -1,3 +1,5 @@
+import ast
+
 from crosshair.fnutil import FunctionInfo
 from crosshair.options import DEFAULT_OPTIONS, AnalysisOptionSet
 from crosshair.path_search import OptimizationKind, path_search
@@ -15,6 +17,8 @@ def test_optimize_options() -> None:
     ret = path_search(fninfo, opts, optimization_kind=OptimizationKind.SIMPLIFY)
     assert ret in ('{"x": 1, "y": 0}', '{"x": 0, "y": 1}')
     ret = path_search(fninfo, opts, optimization_kind=OptimizationKind.MINIMIZE_INT)
-    assert ret == '{"x": 11, "y": 0}'
+    assert ret is not None
+    parsed_ret = ast.literal_eval(ret)
+    assert parsed_ret["x"] - parsed_ret["y"] > 10
     ret = path_search(fninfo, opts, optimization_kind=OptimizationKind.NONE)
     assert ret == '{"x": 42, "y": 0}'
