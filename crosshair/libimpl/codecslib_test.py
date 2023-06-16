0dc65c7e71fd1f446945c1e9e556555939f57c8c
Phillip Schanely <pschanely@gmail.com>
2023-06-16 11:46:26 -0400

    parallelize tests & simplify timeouts

--- crosshair/libimpl/codecslib_test.py
+++ crosshair/libimpl/codecslib_test.py
@@ -51,5 +51,4 @@ def test_decode_e2e():
         """
         return byts.decode("utf-8", errors="strict")
 
-    opts = AnalysisOptionSet(max_iterations=500, per_condition_timeout=20)
-    check_states(f, POST_FAIL, opts)
+    check_states(f, POST_FAIL, AnalysisOptionSet(per_condition_timeout=20))
