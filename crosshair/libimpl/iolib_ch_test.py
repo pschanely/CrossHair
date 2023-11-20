be8a56bc94fb7ee9e25f85c9aa228db753b6d032
Phillip Schanely <pschanely@gmail.com>
2023-11-20 17:07:15 -0500

    Patch StringIO() to return our implementation

--- crosshair/libimpl/iolib_ch_test.py
+++ crosshair/libimpl/iolib_ch_test.py
@@ -1,24 +1,12 @@
 import sys
 from io import StringIO
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 
 import pytest  # type: ignore
 
 from crosshair.core_and_libs import MessageType, analyze_function, run_checkables
 from crosshair.libimpl.iolib import BackedStringIO
-from crosshair.options import AnalysisOptionSet
 from crosshair.test_util import compare_returns
-from crosshair.tracers import is_tracing
-
-# crosshair: max_iterations=40
-
-
-def _maybe_stringio(s: BackedStringIO) -> Union[StringIO, BackedStringIO]:
-    if not is_tracing():
-        sio = StringIO(s._contents, s._newline_mode)
-        sio.seek(s._pos)
-        return sio
-    return s
 
 
 def _do_something(s: Union[StringIO, BackedStringIO], opname: str) -> object:
@@ -47,40 +35,39 @@ def _do_something(s: Union[StringIO, BackedStringIO], opname: str) -> object:
     return None
 
 
-def check_stringio_readlines(s: BackedStringIO, hint: int):
+def check_stringio_readlines(s: StringIO, hint: int):
     """post: _"""
 
     def readlines(s, hint: int):
-        return _maybe_stringio(s).readlines(hint)
+        return s.readlines(hint)
 
     return compare_returns(readlines, s, hint)
 
 
-def check_stringio_writelines(s: BackedStringIO, lines: List[str]):
+def check_stringio_writelines(s: StringIO, lines: List[str]):
     """post: _"""
 
     def writelines(s, lines: List[str]):
-        return _maybe_stringio(s).writelines(lines)
+        retval = s.writelines(lines)
+        return (retval, s.tell(), s.getvalue())
 
     return compare_returns(writelines, s, lines)
 
 
-def check_stringio_seek1(s: BackedStringIO, o1: int, w1: int):
+def check_stringio_seek1(s: StringIO, o1: int, w1: int):
     """post: _"""
 
     def seek_double(s, o1: int, w1: int) -> int:
-        s = _maybe_stringio(s)
         s.seek(o1, w1)
         return s.tell()
 
     return compare_returns(seek_double, s, o1, w1)
 
 
-def check_stringio_seek_seek(s: BackedStringIO, o1: int, w1: int, o2: int, w2: int):
+def check_stringio_seek_seek(s: StringIO, o1: int, w1: int, o2: int, w2: int):
     """post: _"""
 
     def seek_seek(s, o1: int, w1: int, o2: int, w2: int) -> int:
-        s = _maybe_stringio(s)
         s.seek(o1, w1)
         s.seek(o2, w2)
         return s.tell()
@@ -88,13 +75,13 @@ def check_stringio_seek_seek(s: BackedStringIO, o1: int, w1: int, o2: int, w2: i
     return compare_returns(seek_seek, s, o1, w1, o2, w2)
 
 
-def check_stringio_seek_write(s: BackedStringIO, o1: int, w1: int, ws: str):
+def check_stringio_seek_write(s: StringIO, o1: int, w1: int, ws: str):
     """post: _"""
 
-    def seek_write(s, o1: int, w1: int, ws: str) -> int:
-        s = _maybe_stringio(s)
+    def seek_write(s, o1: int, w1: int, ws: str) -> Tuple[int, int, str]:
         s.seek(o1, w1)
-        return s.write(ws)
+        retval = s.write(ws)
+        return (retval, s.tell(), s.getvalue())
 
     return compare_returns(seek_write, s, o1, w1, ws)
 
@@ -104,27 +91,26 @@ def check_stringio_write_newlines(newline_mode: str, ws: str):
 
     def write_newlines(newline_mode: Optional[str], ws: str):
         if newline_mode in (None, "", "\n", "\r", "\r\n"):
-            s = _maybe_stringio(BackedStringIO("", newline_mode))
+            s = BackedStringIO("", newline_mode)
             s.write(ws)
             return s.newlines
 
     return compare_returns(write_newlines, newline_mode, ws)
 
 
-def check_stringio_simple_operation(s: BackedStringIO, opname: str):
+def check_stringio_simple_operation(s: StringIO, opname: str):
     """post: _"""
 
     def simple_operation(s, opname: str) -> object:
-        return _do_something(_maybe_stringio(s), opname)
+        return _do_something(s, opname)
 
     return compare_returns(simple_operation, s, opname)
 
 
-def check_stringio_operation_while_closed(s: BackedStringIO, opname: str):
+def check_stringio_operation_while_closed(s: StringIO, opname: str):
     """post: _"""
 
     def closed_operation(s, opname: str) -> object:
-        s = _maybe_stringio(s)
         s.close()
         return _do_something(s, opname)
 
