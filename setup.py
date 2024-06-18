082d8922bced6ebf022ac4c5db450be05af607dd
Phillip Schanely <pschanely@gmail.com>
2024-06-18 18:12:04 -0400

    Upgrade z3 version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         "packaging",
         "typing-inspect>=0.7.1",
         "typing_extensions>=3.10.0",
-        "z3-solver==4.11.2.0",
+        "z3-solver==4.13.0.0",
         "importlib_metadata>=4.0.0",
         "pygls>=1.0.0",  # For the LSP server
         "typeshed-client>=2.0.5",
