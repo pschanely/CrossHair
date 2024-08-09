2919fa999f94ced428d3a871c6ebb0111a99b688
Phillip Schanely <pschanely@gmail.com>
2024-08-09 15:08:39 -0400

    Remove unused imports

--- crosshair/libimpl/heapqlib_test.py
+++ crosshair/libimpl/heapqlib_test.py
@@ -2,9 +2,6 @@ import heapq
 from typing import List
 
 from crosshair.core import proxy_for_type
-from crosshair.options import AnalysisOptionSet
-from crosshair.statespace import CONFIRMED, MessageType
-from crosshair.test_util import check_states
 from crosshair.tracers import ResumedTracing
 
 
