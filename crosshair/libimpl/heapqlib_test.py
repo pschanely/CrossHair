212a9289df796fd539d06f08141cbe2ce969a7b8
Phillip Schanely <pschanely@gmail.com>
2024-08-11 09:42:26 -0400

    Disable heapq test

--- crosshair/libimpl/heapqlib_test.py
+++ crosshair/libimpl/heapqlib_test.py
@@ -1,10 +1,17 @@
 import heapq
+import sys
 from typing import List
+
+import pytest
 
 from crosshair.core import proxy_for_type
 from crosshair.tracers import ResumedTracing
 
 
+# TODO https://github.com/pschanely/CrossHair/issues/298
+@pytest.mark.skip(
+    reason="heapq get reloaded somehow in parallel ci run, ruining the intercepts",
+)
 def test_heapify(space):
     items = proxy_for_type(List[int], "items")
 
