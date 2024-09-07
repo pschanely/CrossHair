5250fa2fd6ef6ef8ec21f26f1e3a8d2fd3453aa4
Phillip Schanely <pschanely@gmail.com>
2024-09-07 13:46:21 -0400

    Start an smtlib module

---
+++ crosshair/smtlib_test.py
@@ -0,0 +1,14 @@
+import math
+
+import z3
+
+from crosshair.smtlib import parse_smtlib_literal
+
+
+def test_parse_smtlib_literal():
+    assert parse_smtlib_literal(z3.FPVal(1.23, z3.Float64).sexpr()) == 1.23
+    assert math.isnan(parse_smtlib_literal(z3.FPVal(math.nan, z3.Float64).sexpr()))
+    assert parse_smtlib_literal(z3.FPVal(-math.inf, z3.Float64).sexpr()) <= -math.inf
+    negzero = parse_smtlib_literal(z3.FPVal(-0.0, z3.Float64).sexpr())
+    assert negzero == 0
+    assert math.copysign(42, negzero) == -42
