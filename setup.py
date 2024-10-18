c97a1557c6fae84017a14aa029d4fe43a4d6325b
Phillip Schanely <pschanely@gmail.com>
2024-10-17 23:37:24 -0400

    Bump version to 0.0.75

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import Extension, find_packages, setup  # type: ignore
 # the pre-commit checks through check_init_and_setup_coincide.py.
 setup(
     name="crosshair-tool",
-    version="0.0.74",  # Update this in crosshair/__init__.py too
+    version="0.0.75",  # Update this in crosshair/__init__.py too
     author="Phillip Schanely",
     author_email="pschanely+vE7F@gmail.com",
     ext_modules=[
