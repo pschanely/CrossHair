d8dcf16057c98a1565d0e8332f51cbe475f2f424
Phillip Schanely <pschanely@gmail.com>
2025-09-19 23:57:02 +0200

    Fix functools.partial interceptions

--- crosshair/libimpl/functoolslib_test.py
+++ crosshair/libimpl/functoolslib_test.py
@@ -1,18 +1,34 @@
 import functools
+import inspect
 
 from crosshair.core import proxy_for_type, standalone_statespace
 from crosshair.libimpl.builtinslib import LazyIntSymbolicStr
-from crosshair.tracers import NoTracing
+from crosshair.tracers import NoTracing, ResumedTracing
 
 
-def test_partial():
-    with standalone_statespace as space:
-        with NoTracing():
-            abc = LazyIntSymbolicStr(list(map(ord, "abc")))
-            xyz = LazyIntSymbolicStr(list(map(ord, "xyz")))
+def test_partial(space):
+    abc = LazyIntSymbolicStr(list(map(ord, "abc")))
+    xyz = LazyIntSymbolicStr(list(map(ord, "xyz")))
+    with ResumedTracing():
         joiner = functools.partial(str.join, ",")
         ret = joiner([abc, xyz])
         assert ret == "abc,xyz"
+
+
+def test_partial_is_interceptable(space):
+    x = proxy_for_type(str, "x")
+    y = proxy_for_type(str, "y")
+    with ResumedTracing():
+        joiner = functools.partial(str.startswith, x)
+        # Ensure we don't explode
+        list(map(joiner, ["foo", y]))
+
+
+def test_partial_arg_is_inspectable(space):
+    with ResumedTracing():
+        joiner = functools.partial(str.join, ",")
+        assert isinstance(joiner, functools.partial)
+        assert inspect.getdoc(joiner.func) == inspect.getdoc(str.join)
 
 
 def test_reduce():
