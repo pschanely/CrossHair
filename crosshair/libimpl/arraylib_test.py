c12483709f1be6858cd3887b3545b4e235c66744
Phillip Schanely <pschanely@gmail.com>
2026-09-25 19:03:44 -0400

    Fix ranges for I/i/L/l array types, cover .extend

--- crosshair/libimpl/arraylib_test.py
+++ crosshair/libimpl/arraylib_test.py
@@ -1,15 +1,13 @@
-import sys
 from array import array
 
+import pytest
+
+from crosshair.libimpl.arraylib import INT_TYPE_BOUNDS, SymbolicArray
 from crosshair.statespace import MessageType
 from crosshair.test_util import check_states
+from crosshair.tracers import ResumedTracing
 
 TARGET = array("h", [-33, 0])
-
-# The machine values 1 and -2 for typecode "h", in native byte order.
-FROMBYTES_H_DATA = (1).to_bytes(2, sys.byteorder) + (-2).to_bytes(
-    2, sys.byteorder, signed=True
-)
 
 
 def test_mutated_array_compares_equal_to_concrete_array():
@@ -39,91 +37,76 @@ def test_array_with_insertion_compares_equal_to_concrete_array():
     check_states(f, MessageType.POST_FAIL)
 
 
-def test_frombytes_decodes_machine_values():
-    def f(a: array) -> None:
-        """
-        pre: a.typecode == 'h'
-        post: list(a) != [1, -2]
-        """
-        a.frombytes(FROMBYTES_H_DATA)
+@pytest.mark.parametrize("typecode", INT_TYPE_BOUNDS.keys())
+def test_int_type_bounds_match_cpython(typecode):
+    minval, maxval = INT_TYPE_BOUNDS[typecode]
+    array(typecode, [minval, maxval - 1])
+    with pytest.raises(OverflowError):
+        array(typecode, [minval - 1])
+    with pytest.raises(OverflowError):
+        array(typecode, [maxval])
 
-    check_states(f, MessageType.POST_FAIL)
+
+@pytest.mark.parametrize("typecode", INT_TYPE_BOUNDS.keys())
+def test_frombytes_decodes_full_itemsize_values(typecode, space):
+    itemsize = array(typecode).itemsize
+    data = b"\x7f" * itemsize + b"\x80" * itemsize
+    expected = array(typecode)
+    expected.frombytes(data)
+    a = SymbolicArray(typecode, [])
+    with ResumedTracing():
+        a.frombytes(data)
+        assert list(a) == list(expected)
 
 
-def test_frombytes_rejects_misaligned_length():
-    def f(a: array) -> bool:
-        """post: _"""
-        if a.itemsize == 1:
-            return True
-        try:
+def test_frombytes_rejects_misaligned_length(space):
+    a = SymbolicArray("h", [])
+    with ResumedTracing():
+        with pytest.raises(ValueError):
             a.frombytes(b"\x00")
-        except ValueError:
-            return False
-        return True
-
-    check_states(f, MessageType.POST_FAIL)
 
 
-def test_frombytes_rejects_non_bytes():
-    def f(a: array) -> bool:
-        """post: _"""
-        try:
-            a.frombytes([1, 2, 3])  # type: ignore[arg-type]
-        except TypeError:
-            return False
-        return True
-
-    check_states(f, MessageType.POST_FAIL)
-
-
-def test_insert_negative_raises_on_unsigned():
-    def f(a: array) -> bool:
-        """post: _"""
-        if a.typecode not in "BHILQ":
-            return True
-        try:
+def test_insert_negative_raises_on_unsigned(space):
+    a = SymbolicArray("H", [])
+    with ResumedTracing():
+        with pytest.raises(OverflowError):
             a.insert(0, -3)
-        except OverflowError:
-            return False
-        return True
-
-    check_states(f, MessageType.POST_FAIL)
 
 
-def test_setitem_negative_raises_on_unsigned():
-    def f(a: array) -> bool:
-        """post: _"""
-        if a.typecode not in "BHILQ" or len(a) == 0:
-            return True
-        try:
+def test_setitem_negative_raises_on_unsigned(space):
+    a = SymbolicArray("H", [0])
+    with ResumedTracing():
+        with pytest.raises(OverflowError):
             a[0] = -3
-        except OverflowError:
-            return False
-        return True
-
-    check_states(f, MessageType.POST_FAIL)
 
 
-def test_add_different_typecode_raises():
-    def f(a: array) -> bool:
-        """post: _"""
-        if a.typecode == "b":
-            return True
-        try:
-            a + array("b", [1])
-        except TypeError:
-            return False
-        return True
-
-    check_states(f, MessageType.POST_FAIL)
+def test_setitem_checks_index_before_value(space):
+    a = SymbolicArray("H", [])
+    with ResumedTracing():
+        with pytest.raises(IndexError):
+            a[5] = -1
 
 
-def test_add_same_typecode_concatenates():
-    def f(a: array) -> None:
-        """
-        pre: a.typecode == 'h'
-        post: list(a) != [1, 2]
-        """
-        a = a + array("h", [1, 2])
+def test_add_same_typecode_concatenates(space):
+    a = SymbolicArray("h", [1])
+    with ResumedTracing():
+        assert list(a + array("h", [2])) == [1, 2]
 
-    check_states(f, MessageType.POST_FAIL)
+
+def test_type_error_cases(space):
+    a = SymbolicArray("h", [])
+    with ResumedTracing():
+        with pytest.raises(TypeError):
+            a.frombytes([1, 2, 3])  # type: ignore[arg-type]
+        with pytest.raises(TypeError):
+            _ = a + array("b", [1])
+        with pytest.raises(TypeError):
+            a + [1]  # type: ignore[operator]
+        with pytest.raises(TypeError):
+            [1] + a  # type: ignore[operator]
+        with pytest.raises(TypeError):
+            a.extend(array("b", [1]))
+        with pytest.raises(TypeError):
+            a += array("b", [1])
+        with pytest.raises(TypeError):
+            a += [1]  # type: ignore[arg-type]
